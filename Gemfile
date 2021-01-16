7653747e246197811d961cf0b1566d3e3f4737b2
labocho <labocho@penguinlab.jp>
2021-01-16 21:39:30 +0900

    initial commit

---
+++ Gemfile
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+# gem "rails"
+
+
+gem "nokogiri", "~> 1.11"
+gem "icalendar", "~> 2.7"
+gem "aws-sdk-s3", "~> 1.64"
+gem "racc", "1.5.2"
---
+++ weathercal.rb
@@ -0,0 +1,109 @@
+require "open-uri"
+require "nokogiri"
+require "icalendar"
+require "aws-sdk-s3"
+require "stringio"
+
+BUCKET_NAME = ENV.fetch("BUCKET_NAME")
+WEATHER_TEXT = {
+  "晴れ" => "☀",
+  "曇り" => "☁",
+  "雪か雨" => "☃",
+  "雨か雪" => "☃",
+  "雨" => "☂",
+  "雪" => "☃",
+  "後時々" => "/",
+  "後" => "/",
+  "時々" => "/",
+  "一時" => "/",
+  "止む" => "☁",
+}
+
+INDEX_HTML = <<~HTML
+<!doctype html>
+<title>weathercal</title>
+HTML
+
+ERROR_HTML = <<~HTML
+<!doctype html>
+<title>weathercal</title>
+HTML
+
+
+def s3
+  @s3 ||= Aws::S3::Client.new
+end
+
+def save(key, body_as_string)
+  s3.put_object(
+    acl: "public-read",
+    body: StringIO.new(body_as_string),
+    bucket: BUCKET_NAME,
+    key: key,
+  )
+end
+
+def fetch
+  html = OpenURI.open_uri("http://www.jma.go.jp/jp/week/", &:read)
+  Nokogiri.parse(html)
+end
+
+def weather_text(s)
+  WEATHER_TEXT.each {|a, b| s.gsub!(a, b) }
+  s
+end
+
+def update(event:, context:)
+  save("index.html", INDEX_HTML)
+  save("error.html", ERROR_HTML)
+
+  today = Time.now.getlocal("+09:00").to_date
+  doc = fetch
+  forecastlist_trs = doc.css("table.forecastlist tr")
+  first_day = forecastlist_trs[0].css("th")[1].inner_text.to_i
+  first_date = case first_day
+  when today.day
+    today
+  when (today + 1).day
+    today + 1
+  else
+    raise "Unknown day #{first_day} on #{today}"
+  end
+
+  days = (first_date..(first_date + 6)).to_a
+
+  forecastlist_trs[1..].each do |tr|
+    next if tr.css(".forecast").empty?
+
+    data = {}
+    tds = tr.css("td").to_a
+    data[:location] = tds.first.inner_text.strip
+    data[:forecasts] = tds[1..].zip(days).map do |(td, day)|
+      forecast = {}
+      forecast[:day] = day
+      forecast[:weather] = weather_text(td.css("img").first["alt"])
+      forecast[:mintemp] = td.css(".mintemp").first.inner_text.strip.to_i
+      forecast[:maxtemp] = td.css(".maxtemp").first.inner_text.strip.to_i
+      forecast
+    end
+
+    cal = Icalendar::Calendar.new
+    data[:forecasts].each do |f|
+      cal.event do |e|
+        e.dtstart = Icalendar::Values::Date.new(f[:day])
+        e.summary = "#{f[:weather]} #{f[:mintemp]}℃/#{f[:maxtemp]}℃"
+      end
+    end
+    save("#{data[:location]}.ical", cal.to_ical)
+  end
+
+  {
+    statusCode: 200,
+    body: {
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event
+    }.to_json
+  }
+end
+
+
